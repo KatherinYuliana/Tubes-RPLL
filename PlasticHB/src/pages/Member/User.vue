f794753d289f58ce9c8fa13b70a272170b7e7333
Peter JS <103310645+PeterJS123@users.noreply.github.com>
2025-07-24 07:36:53 +0700

    User.vue

--- PlasticHB/src/pages/Member/User.vue
+++ PlasticHB/src/pages/Member/User.vue
@@ -6,7 +6,31 @@ import Navbar from "@/components/Navbar.vue"
   <header>
     <Navbar />
   </header>
-  <div>
-    <h1>Ini adalah halaman User</h1>
+  <div class="user">
+    <img src="../../assets/contoh pp.jpeg" alt="">
+    <div class="detail-user">
+      <h1>Nama User</h1>
+      <p>Detail Disini</p>
+    </div>
   </div>
+  <button>Log out</button>
 </template>
+
+<style scope>
+.user {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  padding: 20px;
+  max-width: 600px;
+}
+
+.user img {
+  margin: 50px;
+  border-radius: 50px;
+  height: 200px;
+  object-fit: cover;
+}
+
+.user .detail-user {}
+</style>
