d03b5f3f6f60a513832b5aa412fd697f39fe6eb6
Peter JS <103310645+PeterJS123@users.noreply.github.com>
2025-07-24 07:33:26 +0700

    Navbar.vue

--- PlasticHB/src/components/Navbar.vue
+++ PlasticHB/src/components/Navbar.vue
@@ -2,13 +2,24 @@
   <nav>
     <ul>
       <li class="logo">
-        <img src="../assets/Logo.png" alt="Logo" />
+        <img src="../assets/Logo Toko/Logo.png" alt="Logo" />
       </li>
       <li><router-link to="/Home">Home</router-link></li>
       <li><router-link to="/Search">Search</router-link></li>
       <li><router-link to="/About">About Us</router-link></li>
       <li><router-link to="/Contact">Contact Us</router-link></li>
-      <li><router-link to="/User">User</router-link></li>
+      <li><router-link to="/User">
+          <div class="pp">
+            <img src="../assets/contoh pp.jpeg" alt="">
+          </div>
+        </router-link></li>
     </ul>
   </nav>
 </template>
+
+<style scope>
+.pp img {
+  border-radius: 40px;
+  height: 25px;
+}
+</style>
